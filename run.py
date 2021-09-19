1e541ff1331893d50650a5e9d4a92d792d2f0746
Priyanha <priyanhap@treinetic.com>
2021-09-19 17:13:34 +0530

    python

---
+++ run.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, render_template, request,jsonify
+import pickle
+from PIL import Image   
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        model = pickle.load(open('sentiment_model.pkl', 'rb'))
+        
+        print(model.predict([request.form.get('description')]))
+        prediction = model.predict([request.form.get('description')])
+                                                                                     
+        if prediction == 0:
+            result = "Negative"
+            emoji = "😶😑😪🥺"
+        else:
+            result = "Positive"
+            emoji = "😂😊😉🤓"
+        print(result)
+        output = result
+    else:
+        emoji = ""
+        output = ""
+    return render_template('index.html', output=output, emoji=emoji)
+
+@app.route('/predict',methods=['GET'])
+def predictAPI():
+    model = pickle.load(open('sentiment_model.pkl', 'rb'))
+    val = model.predict([request.form.get('description')])
+    
+    return jsonify({"result":str(val[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
